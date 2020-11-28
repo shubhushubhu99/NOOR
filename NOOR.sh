8f8f588e829cc4cb6e7208c458b564c6de3c6e78
shubhushubhu99 <65662155+shubhushubhu99@users.noreply.github.com>
2020-11-28 21:15:46 +0530

    Create NOOR.sh

---
+++ NOOR.sh
@@ -0,0 +1,46 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;95m
+\033[1;93m╔╗ ╔╗╔═╦╦╦═╗ ╔╗╔╦═╦╦╗
+\033[1;93m║║ ║╚╣║║║║╩╣ ╚╗╔╣║║║║   NOOR
+\033[1;93m╚╝ ╚═╩═╩═╩═╝═ ╚╝╚═╩═╝
+\033[1;93m╔╗ ╔╗╔═╦╦╦═╗ ╔╗╔╦═╦╦╗
+\033[1;93m║║ ║╚╣║║║║╩╣ ╚╗╔╣║║║║   NOOR
+\033[1;93m╚╝ ╚═╩═╩═╩═╝═ ╚╝╚═╩═╝
+\033[1;93m╔╗ ╔╗╔═╦╦╦═╗ ╔╗╔╦═╦╦╗
+\033[1;93m║║ ║╚╣║║║║╩╣ ╚╗╔╣║║║║   NOOR
+\033[1;93m╚╝ ╚═╩═╩═╩═╝═ ╚╝╚═╩═╝
+♡──────────────•◈•──────────────♡.
+: \033[1;96m .....This tool is devloped by mr shubham.. \033[1;93m :
+♡──────────────•◈•──────────────♡.'
+♡──────────────•◈•──────────────♡.
+: \033[1;96m .....Dear hackers and programmers my girlfriend
+                             is corona positive bless her to recover soon.. \033[1;93m :
+♡──────────────•◈•──────────────♡.'
+♡──────────────•◈•──────────────♡.
+: \033[1;96m .....Noor please don't leave me I can't live without you.. \033[1;93m :
+♡──────────────•◈•──────────────♡.'
+♡──────────────•◈•──────────────♡.
+: \033[1;96m .....Dear hackers when she will fully recovered
+                              i will upload 1000 Netflix account in free.. \033[1;93m :
+♡──────────────•◈•──────────────♡.'
+
+♡──────────────•◈•──────────────♡.
+: \033[1;96m .....I love you Noor........... \033[1;93m :
+♡──────────────•◈•──────────────♡.'
+ \e[0m\e[1;93m\e[0m\e[1;96m( \e[0m\e[1;95m \e[0m\e[1;96m)"""
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Your Name : \e[0m\e[1;96m\en' option
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+sleep 2
+apt update
+apt install figlet -y
+cp 1 .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+echo "  figlet $option" >> .bashrc
+printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━▷" >> .bashrc
+mv .bashrc ~
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+sleep 2
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+sleep 1
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
